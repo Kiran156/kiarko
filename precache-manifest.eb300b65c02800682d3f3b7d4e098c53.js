46fe862ff104ba78a3eb4cdaacc02e6d4d6efa08
kiran <kiran.konagutti@gmail.com>
2020-10-07 17:59:41 +0530

    Updates

---
+++ precache-manifest.eb300b65c02800682d3f3b7d4e098c53.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75776dcfc35ed5b26da4fabd6c1aa2f4",
+    "url": "/kiarko/index.html"
+  },
+  {
+    "revision": "9c9abca342c227b913f3",
+    "url": "/kiarko/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "da6cf9f32022e8240794",
+    "url": "/kiarko/static/css/main.717d0385.chunk.css"
+  },
+  {
+    "revision": "9c9abca342c227b913f3",
+    "url": "/kiarko/static/js/2.1eb41172.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/kiarko/static/js/2.1eb41172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da6cf9f32022e8240794",
+    "url": "/kiarko/static/js/main.e5b307eb.chunk.js"
+  },
+  {
+    "revision": "09ed36ba5d0a7510ea2c",
+    "url": "/kiarko/static/js/runtime-main.2e05c1e5.js"
+  },
+  {
+    "revision": "f39d0b9260f6315f4e4634a5a9d822a2",
+    "url": "/kiarko/static/media/12879.f39d0b92.jpg"
+  },
+  {
+    "revision": "a31df559aa18b7220f20ae06c2119fc1",
+    "url": "/kiarko/static/media/asian.a31df559.jpg"
+  },
+  {
+    "revision": "8307b37ee0091500dfc55caf34422af9",
+    "url": "/kiarko/static/media/birla.8307b37e.jpg"
+  },
+  {
+    "revision": "267689e3b3e1e6afcec37b7d3570acb6",
+    "url": "/kiarko/static/media/buildm.267689e3.jpg"
+  },
+  {
+    "revision": "4fb38e9f118cc30e9ae66ac57b459aa4",
+    "url": "/kiarko/static/media/godtiles.4fb38e9f.jpg"
+  },
+  {
+    "revision": "015cafacc3ce3257da5dce8f6b2712cf",
+    "url": "/kiarko/static/media/kiarko.015cafac.jpg"
+  },
+  {
+    "revision": "9a5d1e6badaf93441d62e524fc691c07",
+    "url": "/kiarko/static/media/pop.9a5d1e6b.jpg"
+  },
+  {
+    "revision": "3396fbe3124d7a73795b1ba8718e8b5b",
+    "url": "/kiarko/static/media/roller.3396fbe3.jpg"
+  },
+  {
+    "revision": "28dc4df222137fb87bd5378124244c34",
+    "url": "/kiarko/static/media/thinner.28dc4df2.jpg"
+  }
+]);
